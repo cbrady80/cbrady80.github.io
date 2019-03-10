6685b1a3462a4b740cc916fd69078b0420637a00
Cheryl Brady Account <cbrady80@byui.edu>
2019-03-10 00:22:13 -0700

    json stuff

---
+++ assignments/lesson-9/scripts/towndata.js
@@ -0,0 +1,44 @@
+var article = document.querySelector('article');
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    var townData = request.response;
+        showData(townData);
+    }
+
+function showData(jsonObj) {
+        var data = jsonObj['towns'];
+            
+        for (var i = 0; i < data.length; i++) {
+            if (i === 0 || i === 1 || i === 4 || i === 5) {
+                continue;
+            }
+            var myArticle = document.createElement('div');
+            var myH5 = document.createElement('h5');
+            var myPara1 = document.createElement('h6');
+            var myPara2 = document.createElement('p');
+            var myPara3 = document.createElement('p');
+            var myPara4 = document.createElement('p');
+            var myPhoto = document.createElement('img');
+
+            myH5.textContent = data[i].name;
+            myPara1.textContent = data[i].motto;
+            myPara2.textContent = 'Year Founded: ' + data[i].yearFounded;
+            myPara3.textContent = 'Current Population: ' + data[i].currentPopulation;
+            myPara4.textContent = 'Average Rainfall: ' + data[i].averageRainfall;
+
+            myArticle.appendChild(myH5);
+            myArticle.appendChild(myPara1);
+            myArticle.appendChild(myPara2);
+            myArticle.appendChild(myPara3);
+            myArticle.appendChild(myPara4);
+            myArticle.appendChild(myPhoto);
+            if (i === 1)
+                myPhoto.src = 'images/home'
+
+            section.appendChild(myArticle);
+        }
+    }
