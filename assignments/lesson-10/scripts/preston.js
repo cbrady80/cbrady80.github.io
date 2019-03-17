f092b38bbe7001d9fd6d1cf2d21d05346a2f68b0
Cheryl Brady Account <cbrady80@byui.edu>
2019-03-17 16:30:51 -0600

    finishing up lesson 10

--- assignments/lesson-10/scripts/preston.js
+++ assignments/lesson-10/scripts/preston.js
@@ -2,15 +2,15 @@
 
 // WEATHER SUMMARY
 var weatherObject = new XMLHttpRequest
-weatherObject.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=93513a5ecb45a4d44be23c0ee39c9db3', true);
+weatherObject.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=93513a5ecb45a4d44be23c0ee39c9db3', true);
 weatherObject.send();
 weatherObject.onload = function () {
 
     var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
-
-    document.getElementById('weatherDesc').innerHTML = weatherInfo.weather[0].description;
+    
     document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('high').innerHTML = weatherInfo.main.temp_max;
     document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
 
@@ -22,3 +22,61 @@ weatherObject.onload = function () {
 }
 
 // FORECAST
+var weatherForecast = new XMLHttpRequest
+weatherForecast.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=93513a5ecb45a4d44be23c0ee39c9db3', true);
+weatherForecast.send();
+weatherForecast.onload = function() {
+
+    var forecastInfo = JSON.parse(weatherForecast.responseText);
+    console.log(forecastInfo);
+
+    var listDate = [];
+    var listTemp = [];
+    var listIconCode = [];
+
+    for(i = 0; i < forecastInfo.list.length; ++i) {
+        time = forecastInfo.list[i].dt_txt;
+        if (time.includes("18:00:00")) {
+
+            //date
+            var date = new Date(forecastInfo.list[i].dt * 1000);
+            var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var findDate = weekday[date.getDay()] + "<br>" + month[date.getMonth()] + " " + date.getDate();
+            listDate.push(findDate);
+
+            //temp
+            var temp = forecastInfo.list[i].main.temp_max;
+            temp = Math.round(temp);
+            listTemp.push(temp);
+
+            //icon
+            var iconCode = forecastInfo.list[i].weather["0"].icon;
+            var icon_path = "https://openweathermap.org/img/w/" + iconCode + ".png";
+            listIconCode.push(icon_path);
+        }
+        continue;
+    }
+
+    //Display forecast date
+    document.getElementById('day1').innerHTML = listDate[0];
+    document.getElementById('day2').innerHTML = listDate[1];
+    document.getElementById('day3').innerHTML = listDate[2];
+    document.getElementById('day4').innerHTML = listDate[3];
+    document.getElementById('day5').innerHTML = listDate[4];
+
+    //Display weather icon
+    document.getElementById('weather_icon1').src = listIconCode[0];
+    document.getElementById('weather_icon2').src = listIconCode[1];
+    document.getElementById('weather_icon3').src = listIconCode[2];
+    document.getElementById('weather_icon4').src = listIconCode[3];
+    document.getElementById('weather_icon5').src = listIconCode[4];
+
+    //Display temp
+    document.getElementById('highTemp1').innerHTML = listTemp[0];
+    document.getElementById('highTemp2').innerHTML = listTemp[1];
+    document.getElementById('highTemp3').innerHTML = listTemp[2];
+    document.getElementById('highTemp4').innerHTML = listTemp[3];
+    document.getElementById('highTemp5').innerHTML = listTemp[4];
+
+}
--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -10,7 +10,7 @@ weatherRequest.onload = function() {
 
   document.getElementById('current-temp').innerHTML = weatherData.main.temp;
   
-  let icon = "https://openweathermap.org/img/w/" + weatherData.weather[0].icon +".png";
+  let icon = "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
   let desc = weatherData.weather[0].description;
 
   document.getElementById('cc-img').setAttribute('src', icon);
