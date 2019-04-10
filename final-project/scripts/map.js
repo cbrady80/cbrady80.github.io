e6611d0bee8b329b20854049645318d6ef175653
Cheryl Brady Account <cbrady80@byui.edu>
2019-04-09 23:49:22 -0600

    trying to get the map right

--- final-project/scripts/map.js
+++ final-project/scripts/map.js
@@ -1,8 +1,8 @@
 //Map for Bethesda
 function bethesdaMap() {
     var mapProp = {
-        center: new google.maps.LatLng(38.9832229, -77.095619),
-        zoom: 12,
+        center: new google.maps.LatLng(38.9832229, -77.0960054),
+        zoom: 15,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
