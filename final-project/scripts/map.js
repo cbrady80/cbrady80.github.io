9dd5995ffd44e71749fa7b8c82f0501266cb3902
Cheryl Brady Account <cbrady80@byui.edu>
2019-04-10 00:03:15 -0600

    last of thanks page updates

--- final-project/scripts/map.js
+++ final-project/scripts/map.js
@@ -2,7 +2,7 @@
 function bethesdaMap() {
     var mapProp = {
         center: new google.maps.LatLng(38.9832229, -77.0960054),
-        zoom: 15,
+        zoom: 17,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
