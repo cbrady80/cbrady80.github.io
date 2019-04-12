5681e6173f6ead06b290341d5c0670080739a6f3
Cheryl Brady Account <cbrady80@byui.edu>
2019-04-12 15:49:57 -0600

    finished up slc temple page

--- final-project/scripts/sldata.js
+++ final-project/scripts/sldata.js
@@ -1,60 +1,49 @@
-var article = document.querySelector('article');
-var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var article = document.querySelector('#templeClosures');
+var requestURL = 'https://cbrady80.github.io/json-temple-info.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function() {
-    var townData = request.response;
-        showData(townData);
+    var slcData = request.response;
+        showData(slcData);
     }
 
-function showData(jsonObj) {
-        var data = jsonObj['towns'];
+    function showData(jsonObj) {
+        var data = jsonObj['temples'];
             
         for (var i = 0; i < data.length; i++) {
-            if ((data[i].name == "Preston" || data[i].name == "Soda Springs" || data[i].name == "Fish Haven") == false) {
+            if (data[i].name != "Salt Lake City") {
                 continue;
             }
             var myArticle = document.createElement('div');
-            var myH5 = document.createElement('h5');
-            var myPara1 = document.createElement('h6');
+            
+            var myPara1 = document.createElement('p');
             var myPara2 = document.createElement('p');
             var myPara3 = document.createElement('p');
             var myPara4 = document.createElement('p');
-            var myPhoto = document.createElement('img');
+            var myPara5 = document.createElement('p');
+            var myPara6 = document.createElement('p');
+            var myPara7 = document.createElement('p');
+            
+            
+            myPara1.textContent = data[i].closures[0];
+            myPara2.textContent = data[i].closures[1];
+            myPara3.textContent = data[i].closures[2];
+            myPara4.textContent = data[i].closures[3];
+            myPara5.textContent = data[i].closures[4];
+            myPara6.textContent = data[i].closures[5];
+            myPara7.textContent = data[i].closures[6];
 
-            myH5.textContent = data[i].name;
-            myPara1.textContent = data[i].motto;
-            myPara2.textContent = 'Year Founded: ' + data[i].yearFounded;
-            myPara3.textContent = 'Current Population: ' + data[i].currentPopulation;
-            myPara4.textContent = 'Average Rainfall: ' + data[i].averageRainfall;
 
-            myArticle.appendChild(myH5);
             myArticle.appendChild(myPara1);
             myArticle.appendChild(myPara2);
             myArticle.appendChild(myPara3);
             myArticle.appendChild(myPara4);
-            myArticle.appendChild(myPhoto);
-            if (data[i].name == "Fish Haven") {
-                myPhoto.src = 'images/home-fishhaven-400.jpg'
-            myPhoto.setAttribute('class', 'homePhoto');
-            myPhoto.setAttribute('alt', 'Fish Haven Photo');
-            myArticle.style.order = "3";
-        }
-            if (data[i].name == "Preston") { 
-                myPhoto.src = 'images/home-preston-400.jpg'
-            myPhoto.setAttribute('class', 'homePhoto');
-            myPhoto.setAttribute('alt', 'Preston Photo');
-            myArticle.style.order = "1";
-        }
-            if (data[i].name == "Soda Springs") {
-                myPhoto.src = 'images/home-sodasprings-400.jpg'
-            myPhoto.setAttribute('class', 'homePhoto');
-            myPhoto.setAttribute('alt', 'Soda Springs Photo');
-            myArticle.style.order = "2";
-        }
-
+            myArticle.appendChild(myPara5);
+            myArticle.appendChild(myPara6);
+            myArticle.appendChild(myPara7);
+            
             article.appendChild(myArticle);
         }
     }
