1c52db91bf304a0f9cd4516f750ecdf86e856cbd
Cheryl Brady <cbrady80@byui.edu>
2023-11-09 19:49:24 -0700

    grrrr

--- assignments/lesson-10/scripts/maps.js
+++
@@ -1,8 +0,0 @@
-//Map for Preston
-function prestonMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(42.0963, -111.8766),
-        zoom: 12,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
